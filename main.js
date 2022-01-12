5dcf70c65c740edb34e152bffcbbf4acaba919ee
Dhr1t1 <82240383+Dhr1t1@users.noreply.github.com>
2022-01-11 20:28:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/dQL461Q9/clown-nose-png-12.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function take_snap(){
+    save('ClownNosePicture.png');
+}
+
+function modelLoaded(){
+    console.log('PoseNet is loaded');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX= "+noseX);
+        console.log("noseY= "+noseY);
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(clown_nose,noseX-15,noseY-20,30,30);
+}
